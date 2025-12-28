4629c10d9feebdd2a1855a67489bb2c577320940
Yoedusvany <yoedusvany@gmail.com>
2025-12-28 18:03:37 -0500

    refactor: simplify agent output parsing by replacing PydanticOutputParser with structured LLM output and removing unused imports.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import os
-
 from dotenv import load_dotenv
 
 load_dotenv()
 
 from langchain_classic import hub
-from langchain_classic.agents.agent import AgentExecutor
+from langchain_classic.agents import AgentExecutor
 from langchain_classic.agents.react.agent import create_react_agent
-from langchain_core.output_parsers.pydantic import PydanticOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnableLambda
 from langchain_ollama import ChatOllama
@@ -18,30 +15,28 @@ from schemas import AgentResponse
 
 tools = [TavilySearch()]
 llm = ChatOllama(model="llama3.2")
+structured_llm = llm.with_structured_output(AgentResponse)
 react_prompt = hub.pull("hwchase17/react")
-output_parser = PydanticOutputParser(pydantic_object=AgentResponse)
 react_prompt_with_format_instructions = PromptTemplate(
     template=REACT_PROMPT_WITH_FORMAT_INSTRUCTIONS,
     input_variables=["input", "agent_scratchpad", "tool_names", "tools"],
-).partial(format_instructions=output_parser.get_format_instructions())
+).partial(format_instructions="")
+
 
 agent = create_react_agent(
-    llm, 
-    tools=tools, 
-    prompt=react_prompt_with_format_instructions
+    llm=llm,
+    tools=tools,
+    prompt=react_prompt_with_format_instructions,
 )
-agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True, handle_parsing_errors=True)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 extract_output = RunnableLambda(lambda x: x["output"])
-parse_output = RunnableLambda(lambda x: output_parser.parse(x))
-
-chain = agent_executor | extract_output | parse_output
+chain = agent_executor | extract_output | structured_llm
 
 
 def main():
-    print("Hello from langchain-course!")
     result = chain.invoke(
         input={
-            "input": "search for 3 job positions for an ai engineer using langchain in the Niagara region on linkedin and list their details"
+            "input": "search for 3 job postings for an ai engineer using langchain in the bay area on linkedin and list their details",
         }
     )
     print(result)
